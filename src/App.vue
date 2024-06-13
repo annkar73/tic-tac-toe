f074f16e2355bf79f4dea1b34c7594dd066d4241
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-06-13 10:13:16 +0200

    add init PlayerForm

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+import PlayerForm from '../src/components/PlayerForm.vue';
+
+const playerNames = ref({ X: '', O: '' });
+
+const setPlayerNames = (names) => {
+  playerNames.value = names;
+};
+
+onMounted(() => {
+  const savedNames = JSON.parse(localStorage.getItem('playerNames'));
+  if (savedNames) {
+    playerNames.value = savedNames;
+  }
+});
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <PlayerForm @set-names="setPlayerNames" />
+    <div v-if="playerNames.X && playerNames.O">
+      <p>Player X: {{ playerNames.X }}</p>
+      <p>Player O: {{ playerNames.O }}</p>
+
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/PlayerForm.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue';
+
+const playerX = ref('');
+const playerO = ref('');
+
+const emit = defineEmits(['set-names']);
+
+const submitNames = () => {
+    const names = { X: playerX.value, O: playerO.value };
+    localStorage.setItem('playerNames', JSON.stringify(names));
+    emit('set-names', names);
+};
+
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="submitNames">
+            <input v-model="playerX" placeholder="Spelare X" required>
+            <input v-model="playerO" placeholder="Spelare O" required>
+            <button type="submit">Starta spelet</button>
+        </form>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
