3fff514c53e3907924ac29124149ce242c14cc84
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-08-04 14:49:59 +0200

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/vue-tictactoe-annkar73/",
+  build: {
+    outDir: 'dist',
+  },
   plugins: [vue()],
   resolve: {
     alias: {
