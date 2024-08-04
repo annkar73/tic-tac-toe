c690a0fcb646c6ce6ce1025ae5c7a1899a001a46
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-08-04 14:59:30 +0200

    update base - correct this time

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tictactoe/",
+  base: "/tic-tac-toe/",
   build: {
     outDir: 'dist',
   },
